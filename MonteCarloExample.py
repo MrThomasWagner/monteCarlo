01415c7576e4800ee28971f546599a0a70e6ed77
Tommy <tommy@tommys-air.home>
2014-10-30 18:22:44 -0400

    cleaing up the .py file

--- MonteCarloExample.py
+++ MonteCarloExample.py
@@ -1,23 +1,12 @@
-
-# coding: utf-8
-
-# In[35]:
-
 import random
 import matplotlib.pyplot as plt
 import math
-get_ipython().magic(u'matplotlib inline')
-
-
-# In[36]:
 
 #just reading in a file
 def get_data(dataFile):
     data_IN = open(dataFile)
     return [line.strip() for line in data_IN]
 
-
-# In[37]:
 
 #trials - number of times to sum a randomnly generated list
 #data - the raw data used to construct each randomnly generated list
@@ -32,8 +21,6 @@ def approx_monte_carlo(trials, data, num_entries):
     return results
 
 
-# In[47]:
-
 #fileIn - file containing the raw data
 #num_entries - the length each randomnly generated list should be
 
@@ -46,20 +33,10 @@ def plot_it(fileIn, num_entries, trials=2000, buckets=50):
     plt.show()
 
 
-# In[48]:
-
 CURRENT_NUM_SAMPLES = 1000
 NUM_NEW_SAMPLES = 500
 TOTAL_NEW_SAMPLES = NUM_NEW_SAMPLES + CURRENT_NUM_SAMPLES
 
 
-# In[53]:
-
 # Here is the main call.  Increasing the trials will generate a smoother bell curve, but will take longer. (Try 100 and 2000 to see the difference)
 plot_it('set1/counts', TOTAL_NEW_SAMPLES, trials=1000)
-
-
-# In[ ]:
-
-
-
